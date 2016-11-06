574d6b945d5e161ee1daabde565245931958ee0f
Antti Kaihola <antti16+python@kaihola.fi>
2016-11-06 16:29:13 +0200

    Added weather metering support

--- pyelcon/valpas.py
+++ pyelcon/valpas.py
@@ -11,16 +11,18 @@ def format_timestamp(t):
     timestamp = pd.Timestamp(t)
     if not timestamp.tz:
         timestamp = timestamp.tz_localize('UTC')
-    return timestamp.strftime('%Y-%m-%dT%H:%M:%S%z')
+    local_timestamp = timestamp.tz_convert('Europe/Helsinki')
+    return local_timestamp.strftime('%Y-%m-%dT%H:%M:%S%z')
 
 
 class Valpas:
     login_url = 'https://www.fortum.com/EAIWebFI_PROD/login.jsp?original_url=%2Fvalpas%2F&langId=0'
 
-    def __init__(self, username, password, metering_point):
+    def __init__(self, username, password, metering_point, weather_metering_point=None):
         self.username = username
         self.password = password
         self.metering_point = metering_point
+        self.weather_metering_point = weather_metering_point
         self.browser = mechanicalsoup.Browser()
         self.logged_in = False
 
@@ -63,5 +65,27 @@ class Valpas:
                    if m['values']]
         df = pd.DataFrame.from_records(records,
                                        columns=['timestamp', 'utc_offset', 'value'])
-        return pd.Series(df.value.values,
-                         df.timestamp + df.utc_offset)
+        return pd.Series(df.value.values, df.timestamp)
+
+    def get_temperature(self, begin, end):
+        if not self.logged_in:
+            self.log_in()
+        url = ('https://www.fortum.com/valpas/api/meteringPoints/WEATHER/{}/series'
+               .format(self.weather_metering_point))
+        response = self.browser.get(
+            url,
+            params={'_': milliepoch_utcnow(),
+                    'customerNumber': self.customer_number,
+                    'products': 'WEATHER_TEMPERATURE',
+                    'resolution': 'MONTHS_AS_HOURS',
+                    'startDate': format_timestamp(begin),
+                    'endDate': format_timestamp(end)})
+        self._get_response = response
+        records = [(pd.Timestamp(m['timestamp']),
+                    pd.Timedelta(m['UTCOffset'], unit='h'),
+                    m['values']['WEATHER_TEMPERATURE#0']['value'])
+                   for m in response.json()
+                   if m['values']]
+        df = pd.DataFrame.from_records(records,
+                                       columns=['timestamp', 'utc_offset', 'value'])
+        return pd.Series(df.value.values, df.timestamp)
