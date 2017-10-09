3d564eeacc238db7d1781cda57858f2246f12115
Antti Kaihola <antti16+python@kaihola.fi>
2017-10-09 08:52:43 +0300

    Stateful browser support from MechanicalSoup >= 0.8.0 is now required

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(name='pyelcon',
       version='0.1.0.dev',
       packages=['pyelcon'],
       install_requires=['pandas>=0.18.1,<2',
-                        'MechanicalSoup>=0.6.0,<1'])
+                        'MechanicalSoup>=0.8.0,<1'])
